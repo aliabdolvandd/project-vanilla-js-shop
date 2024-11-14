cea5e64a0f077818d6fbd5a840a58a32018fb310
ali abdolvandd <aliabdolvandaa76@gmail.com>
2024-11-14 19:13:49 +0330

    build:
    create loading

---
+++ vite-project/main.js
@@ -0,0 +1,6 @@
+import { loading } from "./src/layout/onboarding/loading"
+
+
+
+const app = document.getElementById("app")
+app.append(loading)
--- vite-project/src/layout/onboarding/loading/index.js
+++ vite-project/src/layout/onboarding/loading/index.js
@@ -1,8 +1,36 @@
 import { El } from "../../../script";
+import { svgs } from "../../../svgs";
 
- export const loading = () => {
-    return El ({
-        element:'div',
-        className:""
-    })
- }
+export const logoElement = El({
+    element: "div",
+    children: [
+      El({
+        element: "img",
+        restAttrs: { src: "/images/logo.png", alt: "Logo" },
+        className: "w-8 h-8"
+      }),
+      El({
+        element: "span",
+        children: ["Shoea"],
+        className: "text-xl font-semibold text-gray-800"
+      }),
+    ],
+    className: "flex items-center space-x-2",
+  });
+  
+  const loadingElement = El({
+    element: "div",
+    children: [
+      El({
+        element: "div",
+        className: "animate-spin rounded-full h-8 w-8 border-b-2 border-gray-900",
+      }),
+    ],
+    className: "mt-8",
+  });
+  
+  export const loading = El({
+    element: "div",
+    children: [logoElement, loadingElement],
+    className: "flex flex-col items-center justify-center h-screen",
+  });
